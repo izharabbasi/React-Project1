f9ac86c52b3e088573398b00d3ec5a791ac1fe14
izharabbasi <izharabbasi103@gmail.com>
2020-09-23 01:18:44 +0500

    add dinner component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./dinner";
 import './App.css';
 
 function App() {
---
+++ src/dinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Dinner(props){
+    return(
+    <h1>Today we have chicken karahi</h1>
+    );
+}
+
+export default Dinner
